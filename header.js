5be8d42a1452bf03bc5462f1d3067717e44ed453
Tyler Main <tyler.l.main@gmail.com>
2022-08-15 22:28:55 -0500

    lab complete

---
+++ header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component {
+
+	render() {
+		return (
+			<header className="header">
+			<h1>Getting Started with React</h1>
+			</header>
+		)
+	}	
+}
+
+export default Header;
